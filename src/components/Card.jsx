fdd73cc8b019e4e10ac3cfe89c5a4a42941f4397
Qoopkite <qotodus@naver.com>
2024-06-30 05:07:33 +0900

    <feat>:<로그인 시 홈 화면 구현중>

---
+++ src/components/Card.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { styled } from 'styled-components';
+
+const Card_photo = styled.img`
+  width: 16.75rem;
+  height: 16.813rem;
+  border-radius: 1.25rem;
+`;
+
+const Card_wrapper = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default function Card({ img, name, id }) {
+  const navigate = useNavigate();
+
+  return (
+    <Card_wrapper id={id} onClick={() => navigate(`/user/${id}`)}>
+      <Card_photo src={img} alt={name} />
+    </Card_wrapper>
+  );
+}
